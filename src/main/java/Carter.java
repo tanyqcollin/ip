d4eb9b5734fae0dfddf34e2dc59be42c967e9f67
tanyqcollin <collintanyq@gmail.com>
2024-08-23 10:21:57 +0800

    rename duke.java, add greeting and exit

---
+++ src/main/java/Carter.java
@@ -0,0 +1,12 @@
+public class Carter {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        String greeting = divider + "\n"
+                + "Hello! I'm Carter \n"
+                + "What can I do for you? \n"
+                + divider;
+        String exit = "Bye. Hope to see you again soon! \n" + divider;
+        System.out.println(greeting);
+        System.out.println(exit);
+    }
+}
